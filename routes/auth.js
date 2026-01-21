ee32d66187b57174dff482c839194dbe13f965cd
Bhuvan.R <bhuvan.in>
2026-01-21 21:20:38 +0530

    Remove temporary admin creation route

--- routes/auth.js
+++ routes/auth.js
@@ -8,22 +8,6 @@ const { adminLogin } = require("../controllers/authController");
 router.post("/login", adminLogin);
 
 // TEMP: Create admin (use once)
-router.post("/create-admin", async (req, res) => {
-  try {
-    const { username, password } = req.body;
 
-    const hashed = await bcrypt.hash(password, 10);
-
-    await db.query(
-      "INSERT INTO admin (username, password) VALUES (?, ?)",
-      [username, hashed]
-    );
-
-    res.json({ message: "Admin created successfully" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to create admin" });
-  }
-});
 
 module.exports = router;
